1a0a602b35135c51389305548edbdd4bbd2b7b4c
Ada <byjean94@gmail.com>
2022-09-14 15:13:57 +0200

    Add Props to Text Elements

--- src/Components/Text/index.js
+++ src/Components/Text/index.js
@@ -1,4 +1,8 @@
+import styles from '../index.module.css'
 
 export function Text() {
-    return <p>Helloooooo</p>
+    return <div className={styles.textBox}>
+        <h3 className={styles.heading}>Improve your front-end skills by building projects</h3>
+        <p className={styles.text}>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </div>
 }
